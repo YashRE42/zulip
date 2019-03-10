aba74ca8e2830db76117e50f1a7b3ee3205594ef
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2019-03-10 23:28:30 +0530

    top-nav-bar: Add JS logic for updated navbar.

--- static/js/tab_bar.js
+++ static/js/tab_bar.js
@@ -55,29 +55,25 @@ function make_tab_data() {
         if (filter.has_operator("stream")) {
             stream = filter.operands("stream")[0];
             tabs.push(make_tab(stream, hashed, stream, 'stream'));
-        } else if (filter.has_operator("pm-with") ||
-                   filter.has_operand("is", "private")) {
 
+        } else if (filter.has_operand("is", "private")) {
             tabs.push(make_tab("Private Messages", '#narrow/is/private',
                                undefined, 'private_message '));
 
-            if (filter.has_operator("pm-with")) {
-                var emails = filter.operands("pm-with")[0].split(',');
-                var names = _.map(emails, function (email) {
-                    if (!people.get_by_email(email)) {
-                        return email;
-                    }
-                    return people.get_by_email(email).full_name;
-                });
+        } else if (filter.has_operator("pm-with")) {
+            var emails = filter.operands("pm-with")[0].split(',');
+            var names = _.map(emails, function (email) {
+                if (!people.get_by_email(email)) {
+                    return email;
+                }
+                return people.get_by_email(email).full_name;
+            });
 
-                tabs.push(make_tab(names.join(', '), hashed));
-            }
+            tabs.push(make_tab(names.join(', '), hashed));
 
         } else if (filter.has_operator("group-pm-with")) {
-
             tabs.push(make_tab("Group Private", '#narrow/group-pm-with',
                                undefined, 'private_message '));
-
 
         } else if (filter.has_operand("is", "starred")) {
             tabs.push(make_tab("Starred", hashed));
@@ -99,14 +95,14 @@ function make_tab_data() {
             tabs.push(make_tab("Search results", false));
         }
 
-        // Third breadcrumb item for stream-topic naarrows
-        if (filter.has_operator("stream") &&
-            filter.has_operator("topic")) {
-            var topic = filter.operands("topic")[0];
-            hashed = hash_util.operators_to_hash(ops.slice(0, 2));
+        // Third breadcrumb item for stream-topic narrows
+        // if (filter.has_operator("stream") &&
+        //     filter.has_operator("topic")) {
+        //     var topic = filter.operands("topic")[0];
+        //     hashed = hash_util.operators_to_hash(ops.slice(0, 2));
 
-            tabs.push(make_tab(topic, hashed, null));
-        }
+        //     tabs.push(make_tab(topic, hashed, null));
+        // }
     }
 
     if (tabs.length === 0) {
@@ -129,27 +125,124 @@ exports.colorize_tab_bar = function () {
         stream_name = stream_name.toString();
 
         var color_for_stream = stream_data.get_color(stream_name);
-        var stream_dark = stream_color.get_color_class(color_for_stream);
+        // var stream_dark = stream_color.get_color_class(color_for_stream);
         var stream_light = colorspace.getHexColor(
             colorspace.getLighterColor(
                 colorspace.getDecimalColor(color_for_stream), 0.2));
 
-        if (stream_tab.hasClass("stream")) {
-            stream_tab.css('background-color', color_for_stream);
-            if (stream_tab.hasClass("inactive")) {
-                stream_tab.hover (
-                    function () {
-                        $(this).css('background-color', stream_light);
-                    }, function () {
-                        $(this).css('background-color', color_for_stream);
-                    }
-                );
-            }
-            stream_tab.removeClass(stream_color.color_classes);
-            stream_tab.addClass(stream_dark);
-        }
+        $("#tab_list .hash").css('color', stream_light);
+        $("#tab_list .lock").css('color', stream_light);
+        // stream_tab.css('color', stream_dark);
+        // if (stream_tab.hasClass("stream")) {
+        //     stream_tab.css('color', color_for_stream);
+        //     if (stream_tab.hasClass("inactive")) {
+        //         stream_tab.hover (
+        //             function () {
+        //                 $(this).css('color', stream_light);
+        //             }, function () {
+        //                 $(this).css('color', color_for_stream);
+        //             }
+        //         );
+        //     }
+        //     stream_tab.removeClass(stream_color.color_classes);
+        //     stream_tab.addClass(stream_dark);
+        // }
     }
 };
+
+function hide_stream_details() {
+    $(".stream_description").hide();
+    $(".number_of_users").hide();
+}
+
+function empty_nav_bar() {
+    $("#tab_list").addClass("hidden");
+    hide_stream_details();
+    $(".hash").hide();
+    $(".navbar-lock").hide();
+    $(".navbar-search").removeClass("expanded");
+}
+
+function render_stream_details(stream_term) {
+    $("#tab_list").removeClass("hidden");
+    var stream_name = stream_term.operand;
+    var stream  = stream_data.get_sub_by_name(stream_name);
+    $(".stream_description").text(stream.description);
+    $(".nav_bar_user_count").append("<i class=\"fa fa-user-o\"></i> ");
+    $(".nav_bar_user_count").append(stream.subscriber_count);
+    $(".stream_description").show();
+    $(".number_of_users").show();
+    if (stream.invite_only) {
+        $(".navbar-lock").show();
+    } else {
+        $(".hash").show();
+    }
+}
+
+function close_search_bar() {
+    $("#tab_list").removeClass("hidden");
+    $(".navbar-search").removeClass("expanded");
+}
+
+function open_search_bar() {
+    $("#tab_list").addClass("hidden");
+    $(".navbar-search").addClass("expanded");
+}
+
+function display_navbar_elements() {
+    empty_nav_bar();
+    // Handle initializing and home narrow
+    var filter = narrow_state.filter();
+    if (filter === undefined) {
+        close_search_bar();
+        return;
+    }
+
+    var operator_terms = narrow_state.operators();
+
+    var stream_term = _.find(operator_terms, function (term) { return term.operator === "stream";});
+    // Handle stream narrow
+    if (stream_term) {
+        render_stream_details(stream_term);
+        // Does not return in order to handle stream + search or stream + topic + search
+    }
+
+    // Handle search narrow and ANY multi-filter narrows
+    var search_term = _.find(operator_terms, function (term) { return term.operator === "search";});
+    if (search_term ||
+        operator_terms.length > 1 &&
+        // NOT stream > topic
+        !(operator_terms.length === 2 && stream_term && _.find(operator_terms, function (term) { return term.operator === "topic";}))) {
+        open_search_bar();
+        return; // Stream + search or stream + topic + search would return at this point
+    }
+
+    $("#tab_list").removeClass("hidden");
+
+    var operator_term = operator_terms[0];
+
+    if (operator_term.operator === "is") {
+        // Keep navbar open for is:unread or is:alerted
+        if (operator_term.operand === "unread" || operator_term.operand === "alerted") {
+            open_search_bar();
+        }
+
+        // TODO: show mention and star with custom icons instead of lock icon
+        // hide hash and lock for is:mention and is:starred narrows (displayed without any icons)
+        // if (operator_term.operand === "mentioned" || operator_term.operand === "starred") {
+        // }
+    }
+    // TODO: show PMs with mail icon instead of lock icon
+    // hide hash for private messages (private messages displayed with lock icons)
+    if (operator_term.operator === "pm-with" ||
+    operator_term.operator === "group-pm-with" ||
+    operator_term.operator === "is" && operator_term.operand === "private") {
+        $(".navbar-lock").show();
+    }
+    // hide hash and lock for "sent by X" (displayed without any icons)
+    // if (operator_term.operator === "sender") {
+    // }
+}
 
 function build_tab_bar() {
     var tabs = make_tab_data();
@@ -162,6 +255,7 @@ function build_tab_bar() {
 
     tab_bar.append(rendered);
     exports.colorize_tab_bar();
+    display_navbar_elements();
     tab_bar.removeClass('notdisplayed');
 }
 
