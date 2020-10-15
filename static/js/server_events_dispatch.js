a7d0ae27a2b9d929fdc0b329c090c23ce3d17170
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2020-10-15 12:08:49 +0000

    server_events_dispatch: Refactor realm_bot case to use switch/case.
    This should make no functional changes.

--- static/js/server_events_dispatch.js
+++ static/js/server_events_dispatch.js
@@ -237,18 +237,24 @@ exports.dispatch_normal_event = function dispatch_normal_event(event) {
         }
 
         case "realm_bot":
-            if (event.op === "add") {
-                bot_data.add(event.bot);
-            } else if (event.op === "remove") {
-                bot_data.deactivate(event.bot.user_id);
-                event.bot.is_active = false;
-            } else if (event.op === "delete") {
-                blueslip.info("ignoring bot deletion for live UI update");
-                break;
-            } else if (event.op === "update") {
-                bot_data.update(event.bot.user_id, event.bot);
+            switch (event.op) {
+                case "add":
+                    bot_data.add(event.bot);
+                    settings_users.update_bot_data(event.bot.user_id);
+                    break;
+                case "remove":
+                    bot_data.deactivate(event.bot.user_id);
+                    event.bot.is_active = false;
+                    settings_users.update_bot_data(event.bot.user_id);
+                    break;
+                case "delete":
+                    blueslip.info("ignoring bot deletion for live UI update");
+                    break;
+                case "update":
+                    bot_data.update(event.bot.user_id, event.bot);
+                    settings_users.update_bot_data(event.bot.user_id);
+                    break;
             }
-            settings_users.update_bot_data(event.bot.user_id);
             break;
 
         case "realm_emoji":
