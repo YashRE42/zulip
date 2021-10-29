8c59ec30f00151ac4b199d6dfe7457e76a1face8
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2021-10-29 16:52:12 +0530

    favicon: Use realm icon as browser favicon.
    Previously, it was difficult to distinguish between multiple Zulip
    realms, especially when pinned in the browser, as they all used the
    same, default Zulip favicon. This commit allows users to set the
    favicon to be the same as realm icon. (We still show unread counts
    over the favicon, and the red dot in case of pms, in the same manner
    as before).
    
    There are a couple of ways to produce the base64 data url, some of
    which include making the request to the url using XMLHttpRequest() or
    converting to base64 via canvas.toDataURL(). We chose to use fetch()
    and reader.result respectively.
    
    Implementing this naively would have caused our clients to make a
    fetch() every time their unread count is updated, but we avoid this by
    storing the icon_data_url and simply using the stored value instead of
    generating it repeatedly. This means that we also conditionally need
    to regenerate the icon_data_url if the realm_icon is changed.
    
    The possibility of generating the icon_data_url when the realm_icon is
    uploaded, and making it available through eg page_params instead of
    generating it when it's needed through fetch() (ie precomputing) still
    exists, but it's not clear if it would be worth the cost.
    
    This commit also completes the /help doc describing this user setting.
    
    Fixes: #11752.

--- static/js/favicon.js
+++ static/js/favicon.js
@@ -4,8 +4,12 @@ import render_favicon_svg from "../templates/favicon.svg.hbs";
 
 import * as blueslip from "./blueslip";
 import favicon_font_url from "./favicon_font_url!=!url-loader!font-subset-loader2?glyphs=0123456789KMGT∞!source-sans/TTF/SourceSans3-Bold.ttf";
+import {page_params} from "./page_params";
+import {user_settings} from "./user_settings";
 
 let favicon_state;
+let icon_data_url;
+let current_realm_icon_url;
 
 function load_and_set_favicon(rendered_favicon) {
     favicon_state = {
@@ -23,7 +27,15 @@ function set_favicon() {
     $("#favicon").attr("href", favicon_state.url);
 }
 
-export function update_favicon(new_message_count, pm_count) {
+export function update_alternate_icon() {
+    if (user_settings.realm_icon_as_favicon && page_params.realm_icon_url) {
+        $("#alternate_icon").attr("href", page_params.realm_icon_url);
+    } else {
+        $("#alternate_icon").attr("href", "/static/images/favicon.png?v=4");
+    }
+}
+
+export async function update_favicon(new_message_count, pm_count) {
     try {
         if (favicon_state !== undefined) {
             favicon_state.image.removeEventListener("load", set_favicon);
@@ -39,7 +51,13 @@ export function update_favicon(new_message_count, pm_count) {
         }
 
         if (new_message_count === 0 && pm_count === 0) {
-            $("#favicon").attr("href", "/static/images/favicon.svg?v=4");
+            if (user_settings.realm_icon_as_favicon && page_params.realm_icon_url) {
+                $("#favicon").attr("href", page_params.realm_icon_url);
+                $("#alternate_icon").attr("sizes", "any");
+            } else {
+                $("#favicon").attr("href", "/static/images/favicon.svg?v=4");
+                $("#alternate_icon").removeAttr("sizes");
+            }
             return;
         }
 
@@ -52,13 +70,45 @@ export function update_favicon(new_message_count, pm_count) {
                 ? `${Math.floor(new_message_count / 1e3 ** pow)}${suffix}`
                 : "∞";
         const count_long = count.length > 2;
+
+        // we need to generate a data url, we cannot just pass a href into
+        // the template because we generate an immutable blob from the
+        // template which would not include the rendered image
+        if (user_settings.realm_icon_as_favicon && page_params.realm_icon_url) {
+            // generate new icon_data_url if necessary
+            if (!current_realm_icon_url || current_realm_icon_url !== page_params.realm_icon_url) {
+                const response = await fetch(page_params.realm_icon_url);
+                const reader = new FileReader();
+                reader.readAsDataURL(await response.blob());
+                await new Promise((resolve) => {
+                    reader.addEventListener("load", () => {
+                        resolve();
+                    });
+                });
+                // store icon_data_url and the realm_icon_url we used to generate it
+                icon_data_url = reader.result;
+                current_realm_icon_url = page_params.realm_icon_url;
+            }
+            $("#alternate_icon").attr("sizes", "any");
+        } else {
+            // first, make sure we pass a falsy value to `icon_data_url`,
+            // so that we use the Zulip icon in the template.
+            icon_data_url = "";
+            // next, reset our `current_realm_icon_url`, to ensure that
+            // if update_favicon is called again, but with
+            // `realm_icon_as_favicon` as true (ie if we just check the
+            // settings box), we don't just use "" as our stored
+            // `icon_data_url`, because that would be incorrect.
+            current_realm_icon_url = "";
+            $("#alternate_icon").removeAttr("sizes");
+        }
         const rendered_favicon = render_favicon_svg({
             count,
             count_long,
             have_pm: pm_count !== 0,
             favicon_font_url,
+            icon_data_url,
         });
-
         load_and_set_favicon(rendered_favicon);
     } catch (error) {
         blueslip.error("Failed to update favicon", undefined, error.stack);
